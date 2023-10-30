018b455ff9a2aec3ec2b147c3db997feeee67acc
Đào Duy Thái <daoduythai.business@gmail.com>
2023-10-30 23:12:28 +0700

    created User class

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from "./controllers/auth.js"
 
 /* CONFIGURATIONS*/
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// ROUTES WITH FILES 
+app.post("/auth/register", upload.single("picture"), register);
 
 /*MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
